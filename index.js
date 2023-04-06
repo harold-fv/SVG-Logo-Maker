6100cfa54be90fc35b90d8a6fac9ac4ea076edeb
Harold Gonzales <haroldgonzales@yahoo.com>
2023-04-06 00:54:49 -0700

    Updates the index.js and copies the given svg.test.js and shapes.test.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 const { Triangle, Circle, Square } = require("./lib/shapes");
+const SVG = require("./lib/svg");
 
-
-//Create the Questions and save it to variable const
+////Create the Questions and save it to variable const
 const questions = [
   {
     type: "input",
@@ -28,42 +28,40 @@ const questions = [
     message: "Enter the shape color:",
   },
 ];
-// Creates inquirer package to prompt the user for input based on the questions array
+
+//// Creates inquirer package to prompt the user for input based on the questions array
 inquirer.prompt(questions).then((answers) => {
-    let shape;
-    switch (answers.shape) {
-      case "triangle":
-        shape = new Triangle();
-        break;
-      case "circle":
-        shape = new Circle();
-        break;
-      case "square":
-        shape = new Square();
-        break;
-      }
-  
-      //passing the user's input for the shape color 
+  let shape;
+  switch (answers.shape) {
+    case "triangle":
+      shape = new Triangle();
+      break;
+    case "circle":
+      shape = new Circle();
+      break;
+    case "square":
+      shape = new Square();
+      break;
+  }
+
+    //passing the user's input for the shape color 
       //(stored in answers.shapeColor) to the setColor() method
 
-      shape.setColor(answers.shapeColor);
-  
-      //defines a template to create the SVG content
-      const svgContent = `
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 300 200" width="300" height="200">
-      ${shape.render()}
-      <text x="150" y="150" font-size="36" text-anchor="middle" fill="${answers.textColor}">${answers.text}</text>
-    </svg>
-      `;
-    
-      // creates a new file called logo.svg located on SVG_created_file folder or overwrite the existing file with the specified content.
-      fs.writeFile("./SVG_created_file/logo.svg", svgContent, (err) => {
-        if (err) {
-          console.error("Error writing SVG file:", err);
-        } else {
-          console.log("SVG file created successfully!");
-        }
-      });
-  
-  
+  shape.setColor(answers.shapeColor);
+
+  const svg = new SVG();
+  svg.setShape(shape);
+  svg.setText(answers.text, answers.textColor);
+
+  const svgContent = svg.render();
+
+
+  //Creates and saves the SVG logo file(s) to a folder called examples
+  fs.writeFile(`./examples/${answers.shape}.svg`, svgContent, (err) => {
+    if (err) {
+      console.error("Error writing SVG file:", err);
+    } else {
+      console.log("SVG file created successfully!");
+    }
   });
+});
